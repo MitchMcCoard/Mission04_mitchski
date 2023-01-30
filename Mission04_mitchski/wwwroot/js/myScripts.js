7c7c5bcf9c336b725f1a2719963e1518899cfec5
Mitch McCoard <mitch.mccoard@gmail.com>
2023-01-30 11:13:34 -0700

    Fixed the Letter grade if statmments

--- Mission04_mitchski/wwwroot/js/myScripts.js
+++ Mission04_mitchski/wwwroot/js/myScripts.js
@@ -11,35 +11,35 @@
     var final = $('#final').val() * .1
     var intex = $('#intex').val() * .1
 
-    var ioutput = assgn + group + quiz + midterm + final + intex
+    var ioutput = Math.round( assgn + group + quiz + midterm + final + intex )
     var letter = 'E'
 
     //Determine what grade letter this is
     if (ioutput >= 94) {
         letter = 'A'
-    } else if (ioutput <= 90) {
+    } else if (ioutput >= 90) {
         letter = 'A-'
-    } else if (ioutput <= 87) {
+    } else if (ioutput >= 87) {
         letter = 'B+'
-    } else if (ioutput <= 84) {
+    } else if (ioutput >= 84) {
         letter = 'B'
-    } else if (ioutput <= 80) {
+    } else if (ioutput >= 80) {
         letter = 'B-'
-    } else if (ioutput <= 77) {
+    } else if (ioutput >= 77) {
         letter = 'C+'
-    } else if (ioutput <= 74) {
+    } else if (ioutput >= 74) {
         letter = 'C'
-    } else if (ioutput <= 70) {
+    } else if (ioutput >= 70) {
         letter = 'C-'
-    } else if (ioutput <= 67) {
+    } else if (ioutput >= 67) {
         letter = 'D+'
-    } else if (ioutput <= 64) {
+    } else if (ioutput >= 64) {
         letter = 'D'
-    } else if (ioutput <= 60) {
+    } else if (ioutput >= 60) {
         letter = 'D-'
     }
     
     //This writes the output to the input field
-    $('#output').val("You're grade could be an " + letter + " with " + Math.round( ioutput.toString() ) + '% ')
+    $('#output').val("You're grade could be an " + letter + " with " +  ioutput.toString() ) + '% '
 })
 
